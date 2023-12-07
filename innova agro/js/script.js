99a11e0d326a1033838f541dd86753a7108e791d
Daniele Veronesi <106329790+daniveronesi@users.noreply.github.com>
2023-12-07 17:08:27 -0300

    Add files via upload

---
+++ innova agro/js/script.js
@@ -0,0 +1,13 @@
+//window.sr = scrollReveal({ reset: true });
+
+//sr.reveal('.area-1', {
+// rotate{ x: 0, y: 80, z: 0 },
+//duration: 2500,
+//})
+
+ScrollReveal().reveal('.area-1', { delay: 750 });
+ScrollReveal().reveal('.area-2', { delay: 750 });
+ScrollReveal().reveal('.area-3', { delay: 750 });
+ScrollReveal().reveal('.area-4', { delay: 750 });
+ScrollReveal().reveal('.area-5', { delay: 750 });
+ScrollReveal().reveal('.area-6', { delay: 750 });
